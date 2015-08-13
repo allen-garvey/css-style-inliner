5ef336f3595fdd2cd66a9fb19c16b65d05f7ce01
Allen Garvey <allengarvey@hotmail.com>
2015-08-12 23:14:12 -0400

    format css stylesheet input so that tabs and newlines can be used;use hash for style data for increased efficiency;add constants for input html and css stylesheet paths;add doc comments

--- css-style-inliner.rb
+++ css-style-inliner.rb
@@ -1,17 +1,41 @@
 # replaces html classes with inline css
-# css classes must be written all in one line
-# if you use quotes in font-family, use double quotes
+#only supports class selectors, not elements, ids or nested selectors
+# if you use quotes (e.g. font-family), use double quotes
+INPUT_HTML_PATH = "./test.html"
+STYLESHEET_PATH = "./styles.css"
 
-raw_html = File.read("./test.html")
-styles = File.read("./styles.css")
+# read files
+raw_html = File.read(INPUT_HTML_PATH)
+styles = File.read(STYLESHEET_PATH)
 
+# format the stylesheet one line per class so that the regex will work properly
+styles.gsub!(/[\s\t]/, "").gsub!('}', "}\n")
+
+# put all stylesheet data in hash for effeciency
+styles_hash = Hash.new
+styles.split("\n").each do |line|
+	key = line.scan(/\..*{/)[0].gsub(/[\.{]/,'')
+	style = line.scan(/\{.*\}/)[0].gsub(/[\{\}]/,'')
+	styles_hash[key] = style
+end
+
+# find all class declarations in html
 classes = raw_html.scan(/class=['"].+['"]/)
 
 classes.each do |class_data|
-	class_name = class_data.gsub(/class=['"]|['"]/, '')
-	class_style = "style='#{styles.scan(/\.#{Regexp.escape(class_name)}{.*}/)[0].gsub(/.*{|}/,'')}'"
+	class_names = class_data.gsub(/class=['"]|['"]/, '')
+	class_style = "style='"
+	class_names.split(/\s+/).each do |class_name|
+		begin
+			class_style = class_style + styles_hash[class_name]
+		rescue
+			# don't do anything if class not declared in stylesheet
+		end
+	end
+	class_style = class_style + "'"
 	raw_html.sub!(class_data, class_style)
 end
 
+# output html with inline styles
 puts raw_html
 
