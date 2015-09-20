8539ae46356129c2caac91ccb969255736cdf1e1
Allen Garvey <allengarvey@hotmail.com>
2015-09-19 23:08:30 -0400

    refactor to use nokogiri and css style class to support more css selectors and require less code since xpath is used instead of regex

--- css-style-inliner.rb
+++ css-style-inliner.rb
@@ -1,97 +1,58 @@
 # replaces html classes with inline css
-# only supports a single class selector or element selector; not ids or nested or combined selectors
-# if you use quotes (e.g. font-family), use double quotes
 
-def get_css_classes(element_opening_tag)
-	class_attribute = get_class_attribute(element_opening_tag)
-	if class_attribute.nil?
-		return []
-	end
-	class_names = class_attribute.gsub(/class=['"]|['"]/, '')
-	return class_names.split(/\s+/)
-end
+require 'nokogiri'
+load 'css.rb'
 
-def get_class_attribute(element_opening_tag)
-	return element_opening_tag.scan(/class=['"].+['"]/)[0]
-end
 def strip_css_comments(css_string)
-	return css_string.gsub(/[\n\t]/, '').gsub(/\*\//, "*/\n").gsub(/\/\*.*\*\//, '')
+	css_string.gsub(/[\n\t]/, '').gsub(/\*\//, "*/\n").gsub(/\/\*.*\*\//, '')
+end
+def preprocess_css(css_string)
+	# format the stylesheet one line per class so that the regex will work properly
+	strip_css_comments(css_string).gsub(/[\s\t]/, "").gsub('}', "}\n")
 end
 
 
 INPUT_HTML_PATH = "./test.html"
-STYLESHEET_PATH = "./styles.css"
 
-# read files
-raw_html = File.read(INPUT_HTML_PATH)
-styles = File.read(STYLESHEET_PATH)
+TEMP_STYLE_ATTRIBUTE_NAME = 'inliner_style'
 
-# strip comments
-styles = strip_css_comments(styles)
+# read html and get all linked css
+page = Nokogiri::HTML(open(INPUT_HTML_PATH))
+style_tags = page.css('link[rel=stylesheet]')
+unless style_tags.length > 0
+	raise "No CSS stylesheet link tags found in HTML file"
+end
 
-# format the stylesheet one line per class so that the regex will work properly
-styles.gsub!(/[\s\t]/, "").gsub!('}', "}\n")
+#collect all linked css files style data
+css_styles = []
 
-# put all stylesheet data for classes in hash for effeciency
-class_styles_hash = Hash.new
-element_styles_hash = Hash.new
+style_tags.each do |style_tag|
+	style_sheet_path = style_tag['href']
+	styles = File.read(style_sheet_path)
+	styles = preprocess_css(styles)
 
-styles.split("\n").each do |line|
-	# search for class selectors
-	key = line.scan(/\..*{/)
-	style_hash = nil
-	if key.length > 0
-		key = key[0].gsub(/[\.{]/,'')
-		style_hash = class_styles_hash
-	# must be an element selector then
-	else
-		key = line.scan(/^\s*[a-zA-Z]+.*{/)[0].gsub(/[\.{]/,'')
-		style_hash = element_styles_hash
-	end
-	# extract css style
-	style = line.scan(/\{.*\}/)[0].gsub(/[\{\}]/,'')
-	# test for redundant selectors
-	if style_hash.key?(key)
-		style_hash[key] = style_hash[key] + style
-	else
-		style_hash[key] = style
+	styles.split("\n").each do |line|
+		css_styles << CSS::CSSStyle.new(line)
 	end
 end
 
-#find all elements in html
-element_opening_tags = raw_html.scan(/<[a-zA-Z]+[a-zA-Z0-9\-_';=()&+:"{}\[%$#@!,?\/\\|\].\s\t\n]*>/)
-# puts element_opening_tags
-
-# find all class declarations in html
-classes = raw_html.scan(/class=['"].+['"]/)
-
-element_opening_tags.each do |element_tag|
-	style_attribute = "style='"
-	# get styles for element
-	element = element_tag.scan(/<[a-zA-Z]+/)[0].sub('<', '')
-	style = element_styles_hash[element]
-	# get styles for classes
-	get_css_classes(element_tag).each do |class_name|
-		class_style = class_styles_hash[class_name]
-		if !class_style.nil?
-			style = style + class_style
-		end
-	end
-
-	if !style.nil?
-		# add inline style attribute
-		styled_element_open_tag = element_tag.sub(/>[\s\n\t]*$/, '') + style_attribute + style + "'" + '>'
-		# remove class attribute
-		styled_element_open_tag.gsub!(get_class_attribute(element_tag), '')
-		# replace only first occurrence since we are going through the elements in order
-		raw_html.sub!(element_tag, styled_element_open_tag)
+#apply all styles to temporary attribute so that it won't overwrite existing style attributes so
+#styles can be added according to css priority
+css_styles.each do |css|
+	page.css(css.selector).each do |element|
+		current_temp_style = element[TEMP_STYLE_ATTRIBUTE_NAME] || ''
+		element[TEMP_STYLE_ATTRIBUTE_NAME] = current_temp_style + css.style
 	end
 end
 
-
-
-
+#add temporary attribute styles back to style tag
+page.css('[inliner_style]').each do |element|
+	current_style = element['style'] ||  ''
+	element['style'] =  element[TEMP_STYLE_ATTRIBUTE_NAME] + current_style
+end
+#remove temporary inline styles
+page.xpath("//@#{TEMP_STYLE_ATTRIBUTE_NAME}").remove
 
 # output html with inline styles
-puts raw_html
+puts page
 
